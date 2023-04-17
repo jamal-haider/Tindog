56804adfca1f177a7e6b870f8d0b7a3bc2964552
Jamal Haider <77342903+jamal-haider@users.noreply.github.com>
2023-04-17 06:36:27 +0500

    App completed

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 import dogsData from './data.js'
 import Dog from './Dog.js'
 
-const dogsArray = ['Rex', 'Bella', 'Teddy']
-
-
 function getNewDog(){
-    return dogsData.find(dog => dog.name === dogsArray.shift())
+    const nextDog = dogsData.find(dog => dog.hasBeenSwiped === false)
+    return nextDog ? new Dog(nextDog) : false
 }
 
-const dogData = getNewDog()
-const dog = new Dog(dogData)
+function getNextDog(name){
+    setTimeout(() => {
+        dog = getNewDog()
+        if(dog){
+            render()
+        }
+        else{
+            endDate()
+        }
+    }, 1500);
+}
+
+function endDate(){
+    const likedOnes = dogsData.filter(dog => dog.hasBeenLiked === true)
+    console.log(likedOnes)
+    document.getElementById('main').innerHTML = `<h1>You've liked <br> ${likedOnes.map(dog => dog.name)} </h1>`
+    document.getElementById('footer').style.display = 'none'
+}
+
+let dog = getNewDog()
 
 function render(){
     document.getElementById('main').innerHTML = dog.getDogHTML()
 }
+
+function getCross(){
+    document.getElementById('img-nope').style.display = 'block'
+    dogsData.map(data => {
+        if(data.name === dog.name)
+            data.hasBeenSwiped = true
+    })
+
+    getNextDog()
+}
+
+function getHeart(){
+    document.getElementById('img-like').style.display = 'block'
+    dogsData.map(data => {
+        if(data.name === dog.name){
+            data.hasBeenSwiped = true
+            data.hasBeenLiked = true
+        }
+    })
+    getNextDog()
+}
+
+document.getElementById('icon-cross').addEventListener('click', getCross)
+document.getElementById('icon-heart').addEventListener('click', getHeart)
+
 
 render()
 
